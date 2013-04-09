ea895c4513904cb24659dd9f6f64b1efd7a3700b
Phillip Beeler <phillip.beeler@drake.edu>
2013-04-09 10:28:02 -0500

    created a child list screen

--- src/edu/Drake/babysteps/ChildListActivity.java
+++ src/edu/Drake/babysteps/ChildListActivity.java
@@ -1,15 +1,36 @@
 package edu.Drake.babysteps;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ChildListActivity extends Activity {
+	
+private static final String TAG = "ChildListActivity";
+	
+	Button buttonN;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_child_list);
+		
+		buttonN = (Button) findViewById(R.id.nextButton);
+		buttonN.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//Goes to myListActivity when clicked
+			Log.v(TAG, "button pressed");
+			Intent intent = new Intent(v.getContext(), ChildSettingsActivity.class);
+			startActivity(intent);
+			}
+		});
+
 	}
 
 	@Override
