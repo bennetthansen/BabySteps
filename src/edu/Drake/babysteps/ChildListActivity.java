45f39a814f067b98742d15083c9c294475aa408e
Bennett Hansen <bennett.hansen@gmail.com>
2013-04-22 23:37:58 -0500

    Layout updates

--- src/edu/Drake/babysteps/ChildListActivity.java
+++ src/edu/Drake/babysteps/ChildListActivity.java
@@ -3,10 +3,10 @@ package edu.Drake.babysteps;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 
 public class ChildListActivity extends Activity {
@@ -18,6 +18,8 @@ public class ChildListActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_child_list);
 
